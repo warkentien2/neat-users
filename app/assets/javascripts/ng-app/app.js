48e5b6977d96a17512cfbc9ed24b56a27fd4e427
warkentien2 <philip.dw2@gmail.com>
2015-12-16 02:03:10 -0300

    adicionar angular-material

--- app/assets/javascripts/ng-app/app.js
+++ app/assets/javascripts/ng-app/app.js
@@ -10,3 +10,46 @@ angular
             });
         $locationProvider.html5Mode(true);
     });
+
+/*
+  angular.module('ListTest', ['ngMaterial'])
+  .controller('AppCtrl', function($scope) {
+      $scope.todos = [
+        {
+          face : 'https://placekitten.com/g/200/300',
+          what: 'Brunch this weekend?',
+          who: 'Min Li Chan',
+          when: '3:08PM',
+          notes: " I'll be in your neighborhood doing errands"
+        },
+        {
+          face : 'https://placekitten.com/g/200/300',
+          what: 'Brunch this weekend?',
+          who: 'Min Li Chan',
+          when: '3:08PM',
+          notes: " I'll be in your neighborhood doing errands"
+        },
+        {
+          face : 'https://placekitten.com/g/200/300',
+          what: 'Brunch this weekend?',
+          who: 'Min Li Chan',
+          when: '3:08PM',
+          notes: " I'll be in your neighborhood doing errands"
+        },
+        {
+          face : 'https://placekitten.com/g/200/300',
+          what: 'Brunch this weekend?',
+          who: 'Min Li Chan',
+          when: '3:08PM',
+          notes: " I'll be in your neighborhood doing errands"
+        },
+        {
+          face : 'https://placekitten.com/g/200/300',
+          what: 'Brunch this weekend?',
+          who: 'Min Li Chan',
+          when: '3:08PM',
+          notes: " I'll be in your neighborhood doing errands"
+        },
+      ];
+  });
+*/
